adeceb8d341b1adc32e0566975d26620ddc6f989
huangketong <ketong.hkt@alibaba-inc.com>
2022-08-18 17:47:46 +0800

    feat: 版本检测

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { Command } from 'commander';
 import fse from 'fs-extra';
+import { promisify } from 'util';
+import { exec } from 'child_process';
+import semver from 'semver';
+import chalk from 'chalk';
 import pathUtil from 'path';
 import initJslint from './command/init-js-eslint';
 import initReactEslint from './command/init-react-eslint';
@@ -13,9 +17,57 @@ import installCommitlint from './command/install-commitlint-dep';
 import initTsLint from './command/init-ts-eslint';
 import installTsLint from './command/install-dep-ts';
 
+interface PackageInfo {
+  name: string;
+  'dist-tags': {
+    latest: string;
+  };
+  versions: string[];
+}
+
 function loadPackageInfo() {
   const pkg = fse.readJsonSync(pathUtil.join(__dirname, '../package.json'));
   return pkg;
+}
+
+// 命令行工具的版本检查
+async function notifyUpgrade() {
+  const pkg = loadPackageInfo();
+  global.console.log('正在请求版本更新信息');
+  const res = await promisify(exec)(`npm view ${pkg.name} --json`)
+    .catch((e) => {
+      return { stderr: e.message, stdout: '' };
+    });
+
+  if (res.stderr) {
+    global.console.error(res.stderr);
+    return;
+  }
+
+  const info = parseJson(res.stdout) as PackageInfo;
+  if (!info) {
+    global.console.error('parse json error', res.stdout);
+    return;
+  }
+
+  const latest = info['dist-tags']?.latest;
+  if (semver.lte(latest, pkg.version)) {
+    global.console.log(chalk.green(`已经是最新版本：${latest}`));
+    return;
+  }
+
+  global.console.log(
+    chalk.yellow(`发现新的版本!!! 运行 npm install -g ${info.name}@${latest} 升级`),
+    `当前版本(${pkg.version})`,
+  );
+}
+
+function parseJson(json: string) {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return null;
+  }
 }
 
 function main() {
@@ -23,7 +75,7 @@ function main() {
 
   const pkg = loadPackageInfo();
 
-  program.name('hkt-cli').description('一个命令行工具').version(pkg.version);
+  program.name('hkt-cli').description('一个命令行工具').version(pkg.version).action(notifyUpgrade);
 
   program.command('init-eslint-js').description('初始化 JS 项目 eslint').action(initJslint);
 
