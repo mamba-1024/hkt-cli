b05fe060aff8b674ba2615cebb510b6849f7edc7
huangketong <ketong.hkt@alibaba-inc.com>
2022-10-25 15:51:35 +0800

    chore: 更新项目配置的husky&移除pre-commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
-      2,  // 要求提交类型
+      2, // 要求提交类型
       'always',
       [ // 允许的提交类型
         'feat', // 新功能
@@ -27,5 +27,5 @@ module.exports = {
     'scope-empty': [0],
     'subject-min-length': [2, 'always', 4], // 提交标题最小长度
     'subject-max-length': [2, 'always', 100], // 提交标题最大长度
-  }
+  },
 };
