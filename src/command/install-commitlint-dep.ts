d27b00160d1bc27dda4f6abfa992262c2b6aee1a
huangketong <ketong.hkt@alibaba-inc.com>
2022-10-25 17:58:45 +0800

    feat: 更新commitlint去掉pre-commit

--- src/command/install-commitlint-dep.ts
+++ src/command/install-commitlint-dep.ts
@@ -2,7 +2,7 @@ import { installDeps } from './util';
 
 export default function () {
   const installTip =
-    'npm install --save-dev lint-staged pre-commit @commitlint/cli @commitlint/config-conventional husky';
+    'npm install --save-dev lint-staged @commitlint/cli @commitlint/config-conventional husky';
 
   installDeps(installTip);
 }
